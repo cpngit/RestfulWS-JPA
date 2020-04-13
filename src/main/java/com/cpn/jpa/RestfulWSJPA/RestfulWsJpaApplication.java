ee55be2d523f63c2e041fb8ff7fd48d979df54e8
cassionunes@cofcointernational.com <cassionunes@cofcointernational.com>
2020-04-13 20:45:09 +0100

    First Start Spring IO with JPA and H2

---
+++ src/main/java/com/cpn/jpa/RestfulWSJPA/RestfulWsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.cpn.jpa.RestfulWSJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWsJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWsJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpn/jpa/RestfulWSJPA/RestfulWsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpn.jpa.RestfulWSJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
